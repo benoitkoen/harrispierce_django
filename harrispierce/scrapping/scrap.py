8f79970c263ec5eff815362f7e7d32663c11f22d
benoitkoen <benoit.koenig@chauffeur-prive.com>
2017-07-27 21:19:30 +0200

    BENOIT scrap les echos et try article

--- harrispierce/scrapping/scrap.py
+++ harrispierce/scrapping/scrap.py
@@ -1,4 +1,4 @@
-from scrapping_functions import scrapwsj1, scrapwsj2, scrapwsj3, scrapft, scrapnyt1, scrapnyt2
+from scrapping_functions import scrapwsj1, scrapwsj2, scrapwsj3, scrapwsj4, scrapft, scrapnyt1, scrapnyt2, scraple, scraparticle
 
 import pandas as pd
 pd.set_option('display.width', 320)
@@ -11,11 +11,14 @@ def scrap(journal, section, url):
     if (journal == 'Wall Street Journal') & (section in ['World', 'Companies', 'Opinion']):
         res = pd.DataFrame(scrapwsj1(url))
 
-    elif (journal == 'Wall Street Journal') & (section in ['Politics', 'Tech']):
+    elif (journal == 'Wall Street Journal') & (section in ['Tech']):
         res = pd.DataFrame(scrapwsj2(url))
 
     elif (journal == 'Wall Street Journal') & (section in ['Economy']):
         res = pd.DataFrame(scrapwsj3(url))
+
+    elif (journal == 'Wall Street Journal') & (section in ['Politics']):
+        res = pd.DataFrame(scrapwsj4(url))
 
     elif journal == 'Financial Times':
         res = pd.DataFrame(scrapft(url))
@@ -26,16 +29,24 @@ def scrap(journal, section, url):
     elif (journal == 'New York Times') & (section in ['Dealbook']):
         res = pd.DataFrame(scrapnyt2(url))
 
+    elif journal == 'Les Echos':
+        res = pd.DataFrame(scraple(url))
+
     return res
 
 
 #print('yo')
-#t = scrap('Financial Times', 'World', 'https://www.ft.com/world')
+t = scrap('Financial Times', 'World', 'https://www.ft.com/world')
 
 #t = scrap('New York Times', 'Dealbook', 'http://www.nytimes.com/pages/business/dealbook/index.html?src=busfn')
-t = scrap('Wall Street Journal', 'Tech', 'https://www.wsj.com/news/technology')
+#t = scrap('Wall Street Journal', 'Tech', 'https://www.wsj.com/news/technology')
+#t = scrap('Wall Street Journal', 'Economy', 'https://www.wsj.com/news/economy')
+#t = scrap('Wall Street Journal', 'Tech', 'https://www.wsj.com/news/technology')
 
 
-print(t)
-print(t.iloc[0]['images'])
+#print(t)
+print(t.iloc[1]['hrefs'])
+
+
+print(scraparticle(t.iloc[1]['hrefs']))
 
