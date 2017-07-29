fe2384f76fb8438d4cf4a2d40e07f63f9a675e95
benoitkoen <benoit.koenig@chauffeur-prive.com>
2017-07-29 12:36:42 +0200

    BENOIT files for settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn harrispierceDjango.wsgi --log-file -
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/benoitkoenig/Desktop/divers/WEB/django/harrispierce_django/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
