013607dfe261ded9d79ea8b5803a4c3f95eaf5d8
Benoit Koenig <benoitkoen@gmail.com>
2017-09-19 18:05:08 -0400

    History (#17)
    * BENOIT select latest choices
    
    * BENOIT history in index for loggedin users

--- harrispierce/views_functions.py
+++ harrispierce/views_functions.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from datetime import datetime
+from collections import defaultdict
 import psycopg2
 
 from .models import Article, Journal, Section
@@ -13,11 +14,17 @@ database = DATABASES['default']['NAME']  # 'postgres'
 myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
 
-def index_get():
+def index_get(user):
     journals = Journal.objects.prefetch_related('sections').all()
-    args = {'journals': journals}
 
-    return journals, args
+    choices = None
+
+    if user is not None:
+        choices = get_choices(myConnection, user)
+
+    args = {'journals': journals, 'choices': choices}
+
+    return args
 
 
 def display_get(selection, user):
@@ -63,7 +70,7 @@ def display_search(keyword, sources, date, quantity):
 
 
 def insert_choices(conn, selection_dict, user):
-    print('user: ', user)
+
     choices = {}
     for journal in selection_dict.keys():
         choices[journal] = list(selection_dict[journal].keys())
@@ -82,10 +89,15 @@ def insert_choices(conn, selection_dict, user):
     for journal in selection_dict.keys():
         for section in selection_dict[journal]:
 
-            cur.execute('SELECT id FROM harrispierce_journal WHERE name = {}{}{}'.format("'", journal, "'"))
-            journal_id = cur.fetchone()[0]
-            cur.execute('SELECT id FROM harrispierce_section WHERE name = {}{}{}'.format("'", section, "'"))
-            section_id = cur.fetchone()[0]
+            cur.execute('SELECT jname, journal_id, name, section_id '
+                        'FROM harrispierce_section S, (select J.id as Jid, J.name as Jname, JS.journal_id as journal_id, JS.section_id as section_id FROM harrispierce_journal J '
+                        'LEFT JOIN harrispierce_journal_sections JS ON J.id = JS.journal_id) AS joi '
+                        'WHERE S.id = joi.section_id and jname = {}{}{} and name = {}{}{}'.format("'", journal, "'", "'", section, "'"))
+            res = cur.fetchone()
+
+            journal_id = res[1]
+            section_id = res[3]
+            
             cur.execute('SELECT id FROM auth_user WHERE username = {}{}{}'.format("'", user, "'"))
             user_id = cur.fetchone()[0]
 
@@ -95,3 +107,49 @@ def insert_choices(conn, selection_dict, user):
                          section_id,
                          datetime.utcnow(),
                          ))
+
+            print('WOOOOOOOO', res)
+
+
+def get_choices(conn, user):
+
+    choices = defaultdict(list) #{}
+
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute('SELECT id FROM auth_user WHERE username = {}{}{}'.format("'", user, "'"))
+    user_id = cur.fetchone()[0]
+
+    cur.execute("SELECT date_trunc('seconds', choice_date) FROM userprofile_choice WHERE user_id = {} ORDER BY date_trunc('seconds', choice_date) DESC limit 1".format(user_id))
+    latest = cur.fetchone()
+    if latest is None:
+        return choices
+    latest = latest[0]
+
+    cur.execute("SELECT choice_date, journal_id, section_id FROM userprofile_choice WHERE user_id = %s and date_trunc('seconds', choice_date) = %s", [user_id, latest])
+
+    result = cur.fetchall()
+
+    for row in result:
+        journal = row[1]
+        section = row[2]
+
+        if journal not in choices['journal']:
+            choices['journal'].append(journal)
+        if section not in choices['section']:
+            choices['section'].append(section)
+
+    """
+    for row in result:
+        journal = row[1]
+        section = row[2]
+
+        if journal not in choices.keys():
+            choices[journal] = []
+            choices[journal].append(section)
+        else:
+            choices[journal].append(section)
+    """
+    print(choices)
+    return choices
