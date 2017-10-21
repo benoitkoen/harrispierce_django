e6e0a410cbc68e87416b543bd2a9752a637943d7
Benoit Koenig <benoitkoen@gmail.com>
2017-10-21 12:05:40 +0200

    BENOIT add user profile app (#19)
    Integrate user profile app to project

--- harrispierce/views_functions.py
+++ harrispierce/views_functions.py
@@ -16,6 +16,12 @@ myConnection = psycopg2.connect(host=hostname, user=username, password=password,
 
 
 def index_get(user):
+    """
+    :param user: The session user.
+    :return: A dict containing:
+    - journals which is a queryset of Journal objects (with related Section objects)
+    - choices which is a dict: {'journal': [1, 2], 'section': [1, 2, 7]}
+    """
     journals = Journal.objects.prefetch_related('sections').all()
 
     choices = None
@@ -29,6 +35,13 @@ def index_get(user):
 
 
 def display_get(selection, user):
+    """
+    :param selection: A list containing 'journal_id-section_id' select by the user in index.
+    :param user: The session user.
+    :return: A dict containing:
+    - selection_dict which is a dict: {Journal object: {Section object: Article queryset}}
+    - pinned_articles which is a Article queryset
+    """
 
     selection_dict = {}
 
@@ -59,6 +72,14 @@ def display_get(selection, user):
 
 
 def display_search(keyword, sources, date, quantity):
+    """
+    :param keyword: A text input.
+    :param sources: A checkbox for each journal.
+    :param date: A date after which articles are retrieved.
+    :param quantity: An integer input for the number of articles to retrieve per journal.
+    :return: A dict containing:
+    - selection_dict which is a dict with journal_name as key and Article queryset as value
+    """
     selection_dict = {}
 
     for journal in sources:
@@ -69,14 +90,21 @@ def display_search(keyword, sources, date, quantity):
             journal_id__name=journal,
         ).order_by('pub_date')[:quantity]
 
-        selection_dict[journal] = articles
+        if len(articles) > 0:
+            selection_dict[journal] = articles
 
+    print('dsjnjns ', selection_dict)
     args = {'selection_dict': selection_dict}
 
     return args
 
 
 def insert_choices(selection, user):
+    """
+    :param selection: A list containing 'journal_id-section_id' select by the user in index.
+    :param user: The session user.
+    :return: Creates a Choice object recording which journal and section a user has chosen in index.
+    """
 
     for journal_section in selection:
         journal_id, section_id = journal_section.split('-')
@@ -92,6 +120,11 @@ def insert_choices(selection, user):
 
 
 def get_choices(conn, user):
+    """
+    :param conn: A psycopg2 connection
+    :param user: The session user.
+    :return: A dict containing the latest user's choice: {'journal': [1, 2], 'section': [1, 2, 7]}.
+    """
 
     choices = defaultdict(list)
 
@@ -119,7 +152,5 @@ def get_choices(conn, user):
             choices['journal'].append(journal)
         if section not in choices['section']:
             choices['section'].append(section)
-
-    #articles = Article.objects.filter(journal_id__name=journal, section_id__name=section).order_by('pub_date')[:9]
 
     return choices
