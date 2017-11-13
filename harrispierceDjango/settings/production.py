9746cc6e6786efc849caa4a5ff57e12fc5699b9d
Benoit Koenig <benoitkoen@gmail.com>
2017-11-13 13:41:09 +0100

    BENOIT heroku config (#21)

--- harrispierceDjango/settings/production.py
+++ harrispierceDjango/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = Config.secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['harrispierce.herokuapp.com', 'harrispierce.com']
+ALLOWED_HOSTS = ['harrispierce.herokuapp.com', '.harrispierce.com']
 
 
 # Application definition
